795c9084ae98443e54f4d7033254f891a84de1aa
오동건 <odong-geon@odong-geon-ui-MacBookPro.local>
2022-09-17 14:15:47 +0900

    file command system & global Theme colkr Setting Clear

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "antd/dist/antd.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 
 const AppShell = ({ Component }) => {
   return (
     <>
+      <Header />
       <Component />
+      <Footer />
     </>
   );
 };
